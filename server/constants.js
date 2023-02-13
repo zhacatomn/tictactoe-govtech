80cf7069b029890b1c98785d7ccfbd7dddc4f10d
zhacatomn <zhacatomn@gmail.com>
2023-02-13 16:59:58 +0800

    Add styling and constants

--- server/constants.js
+++ server/constants.js
@@ -2,5 +2,12 @@ const PLAYER_TOKENS = ["X", "O"];
 const EMPTY_SPACE = "";
 const NUM_ROWS = 3;
 const NUM_COLS = 3;
+const ROOM_ID_LENGTH = 5;
 
-module.exports = { PLAYER_TOKENS, EMPTY_SPACE, NUM_ROWS, NUM_COLS };
+module.exports = {
+  PLAYER_TOKENS,
+  EMPTY_SPACE,
+  NUM_ROWS,
+  NUM_COLS,
+  ROOM_ID_LENGTH,
+};
